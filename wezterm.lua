2a0c7fc31086bec92d256ac8b6f66547f73e2d60
garg10may <garg10may@users.noreply.github.com>
2026-03-17 19:08:16 +0530

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,35 +1,30 @@
 local wezterm = require 'wezterm'
-local config = {}
+local config = wezterm.config_builder()
 
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+-- 1. THE ENGINE (Now pointing to Ubuntu)
+config.default_prog = { 'wsl.exe', '~', '-d', 'Ubuntu' }
 
--- 1. THE ENGINE
-config.default_prog = { "C:/msys64/usr/bin/fish.exe", "--login" }
-
--- 2. THE ENVIRONMENT
+-- 2. THE ENVIRONMENT (Cleaned for WSL)
 config.set_environment_variables = {
-  MSYS2_PATH_TYPE = "inherit", 
-  XDG_CONFIG_HOME = "C:/Users/garg1/.config",
-  HOME = "C:/Users/garg1",
+  -- We don't need MSYS2 paths anymore!
+  TERM = "xterm-256color",
 }
 
 -- 3. VISUALS & FONTS
 config.color_scheme = 'AdventureTime'
-config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Bold', italic = false })
-config.font_size = 10
+config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Bold' })
+config.font_size = 11.5 -- Bumped slightly for readability
 config.line_height = 1.1
+config.use_fancy_tab_bar = false
+config.window_padding = { left = 5, right = 5, top = 5, bottom = 0 }
 
--- 4. STATUS BAR (Right Side)
+-- 4. STATUS BAR (Simplified for Linux)
 wezterm.on('update-right-status', function(window, pane)
-  local date = wezterm.strftime('%Y-%m-%d %H:%M:%S')
+  local date = wezterm.strftime('%H:%M')
   local cwd = pane:get_current_working_dir()
-  local home = "file:///C:/Users/garg1"
-  
   local display_cwd = ""
   if cwd then
-    display_cwd = tostring(cwd):gsub(home, "~")
+    display_cwd = tostring(cwd.file_path):gsub("^/home/[^/]+", "~")
   end
 
   window:set_right_status(wezterm.format({
@@ -40,18 +35,19 @@ wezterm.on('update-right-status', function(window, pane)
   }))
 end)
 
+-- 5. THE KEYBOARD COCKPIT
 config.keys = {
-  -- TABS (Chrome/VS Code Standard)
+  -- TABS (Project Management)
   { key = 't', mods = 'CTRL',       action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
   { key = 'w', mods = 'CTRL',       action = wezterm.action.CloseCurrentPane { confirm = true } },
   { key = 'Tab', mods = 'CTRL',     action = wezterm.action.ActivateTabRelative(1) },
   { key = 'Tab', mods = 'CTRL|SHIFT', action = wezterm.action.ActivateTabRelative(-1) },
 
-  -- SPLITS (Visual Standard: | for Vertical line, _ for Horizontal line)
+  -- SPLITS (Horizontal for Terminal Drawer)
   { key = '|', mods = 'CTRL|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
   { key = '_', mods = 'CTRL|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
 
-  -- NAVIGATION (Vim HJKL Standard)
+  -- NAVIGATION (Alt + hjkl)
   { key = 'h', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Left' },
   { key = 'l', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Right' },
   { key = 'k', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Up' },
@@ -59,7 +55,6 @@ config.keys = {
 
   -- PRO TOOLS
   { key = 'z', mods = 'ALT', action = wezterm.action.TogglePaneZoomState },
-  { key = ' ', mods = 'CTRL|SHIFT', action = wezterm.action.QuickSelect },
   { key = 'f', mods = 'CTRL|SHIFT', action = wezterm.action.Search { CaseSensitiveString = "" } },
 }
 
